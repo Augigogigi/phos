419bd72682c8294e86889b226f8df051805913e4
TGR <account@thomasreddy.com>
2023-01-23 20:18:22 +0000

    doc

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # phos
+[https://img.shields.io/badge/docs.rs-phos-9cf?style=for-the-badge&logo=rust](https://docs.rs/phos/latest/phos/)
+[https://img.shields.io/badge/Github-Augigogigi/phos-9cf?style=for-the-badge&logo=github](https://github.com/Augigogigi/phos/)
 A rust crate for a regex-like pattern matcher/permutation generator.
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-//! `phos` is a powerful and flexible rust crate for pattern matching and pattern permutation generation.
+//! A crate for pattern matching and pattern permutation generation.
+//!
 //! It offers a different approach to pattern matching by providing a `Pattern` or `Matcher` struct (similar to a regex expression) that is generic, allowing you to match across any type, not just Strings.
 
 // Imports:
@@ -13,6 +14,8 @@ pub(crate) enum Fragment<T: Clone + PartialEq + Eq> {
 	// TODO: better thing than Vec<MFrag<T>>? box?
 	Quantifier(Vec<Fragment<T>>, Range<usize>, bool),
 	// lazy expands the quantity until match, greedy starts at pat.len() and shrinks until match
+	// TODO: improve
+	Except(Vec<Fragment<T>>, Vec<Fragment<T>>),
 	Unit(T),
 	Class(usize),
 	Or(Vec<Fragment<T>>),
@@ -23,24 +26,47 @@ pub(crate) enum Fragment<T: Clone + PartialEq + Eq> {
 
 /// A pattern struct used to match against other patterns. This is NOT used to generate permutations.
 #[derive(Clone, PartialEq, Eq, Debug)]
-pub struct Matcher<T: Clone + PartialEq + Eq>(Box<Fragment<T>>);
+pub struct Matcher<T: Clone + PartialEq + Eq>(Vec<Fragment<T>>);
+
+impl<T: Clone + PartialEq + Eq> Matcher<T> {
+	fn new() -> Self {
+		Matcher::default()
+	}
+}
 
 impl<T: Clone + PartialEq + Eq> Default for Matcher<T> {
 	fn default() -> Self {
 		Self {
-			0: Box::new(Fragment::None)
+			0: vec![]
 		}
 	}
 }
 
 /// The main difference between `Pattern` and `Matcher` is the lack of the `Any` and `Quantifier` fragments. `Pattern`s can always be promoted to `Matcher`s, but not vice-versa.
 #[derive(Clone, PartialEq, Eq, Debug)]
-pub struct Pattern<T: Clone + PartialEq + Eq>(Box<Fragment<T>>);
+pub struct Pattern<T: Clone + PartialEq + Eq>(Vec<Fragment<T>>);
+
+impl<T: Clone + PartialEq + Eq> Pattern<T> {
+	fn new() -> Self {
+		Pattern::default()
+	}
+}
 
 impl<T: Clone + PartialEq + Eq> Default for Pattern<T> {
 	fn default() -> Self {
 		Self {
-			0: Box::new(Fragment::None)
+			0: vec![]
 		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use crate::{Matcher, Pattern};
+
+	#[test]
+	fn new_is_default() {
+		assert_eq!(Matcher::<u8>::new(), Matcher::<u8>::default());
+		assert_eq!(Pattern::<u8>::new(), Pattern::<u8>::default());
 	}
 }
